83d3a0f6720c8a7b6f74ad9df1454b1e58bb51e8
juanbrutti815 <bruttijuan8@gmail.com>
2025-04-03 18:41:46 -0300

    Add files via upload

---
+++ juannumeros.py
@@ -0,0 +1,23 @@
+# Pedir al usuario que ingrese dos valores
+num1 = float(input("Ingrese el primer numero: "))
+num2 = float(input("Ingrese el segundo numero: "))
+
+# Compara los valores
+if num1 > num2:
+    print("El numero mayor es:num1")
+elif num2 > num1:
+    print("El numero mayor es:num2 ")
+else:
+    print("Los numeros son iguales.")
+        
+        
+         
+
+
+    
+
+
+
+
+
+
