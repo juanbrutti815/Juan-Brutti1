7e4e47c963888a1b54779a1d625ff45924e7d24f
juanbrutti815 <bruttijuan8@gmail.com>
2025-04-03 19:28:03 -0300

    Add files via upload

---
+++ juanfrutas.py
@@ -0,0 +1,18 @@
+fruta = ["manzana","pera","uva","mandarina"]
+fruta.append("banana")
+fruta.insert(2,"durazno")
+fruta.remove("uva")
+fruta.sort()
+fruta.reverse()
+
+
+#while #repite un bloque mientras una condicion sea verdadera
+# contador = 3 //asigna un valor a la variable
+# contador # 5 //hacer comparacion
+
+contador=0
+while True:
+    print(contador)
+    contador +=1
+    if contador == 5:
+        break
